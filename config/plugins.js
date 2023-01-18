c956b534be35b97d53056c88ed3a744b28c04ce3
Adam Szymański <adamszymanski27@hotmail.com>
2023-01-18 14:18:52 +0100

    Add navigation plugin and edit other files

--- config/plugins.js
+++ config/plugins.js
@@ -155,6 +155,22 @@ module.exports = () => {
           },
         }
       }
+    },
+    navigation: {
+      enabled: true,
+      config: {
+          additionalFields: ['audience', { name: 'my_custom_field', type: 'boolean', label: 'My custom field' }],
+          contentTypes: ['api::page.page'],
+          contentTypesNameFields: {
+              'api::page.page': ['title']
+          },
+          allowedLevels: 2,
+          slugify: {
+            customReplacements: [
+              ["🤔", "thinking"],
+            ],
+          }
+      }
     }
   }
 }
